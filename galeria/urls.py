a74cfeeb6579c15763f499d607ef9630cbb6ed5b
Guilherme <guilhermelemes08@gmail.com>
2023-02-15 17:41:01 -0300

    Novo template e configuração de arquivos estáticos

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index)  # A rota para pagina principal chama o metodo index
+    path('', index),  # A rota para pagina principal chama o metodo index
+    path('imagem/', imagem)
 ]
