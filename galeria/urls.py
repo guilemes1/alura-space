8a48fc71ca73d896ee576b22577622237abf52eb
Guilherme <guilhermelemes08@gmail.com>
2023-02-16 15:01:45 -0300

    Nomeando URL's e embedding para paginação

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),  # A rota para pagina principal chama o metodo index
-    path('imagem/', imagem)
+    path('', index, name='index'),  # A rota para pagina principal chama o metodo index
+    path('imagem/', imagem, name='imagem'),
 ]
